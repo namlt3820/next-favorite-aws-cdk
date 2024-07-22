412160523c929170ada4ddc16e87cb56333ec1c7
Le Trung Nam <namle@pegaxy.io>
2024-07-22 11:18:37 +0700

    feat: user pool

--- bin/next-favorite.ts
+++ bin/next-favorite.ts
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { NextFavoriteStack } from '../lib/next-favorite-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { NextFavoriteStack } from "../lib/next-favorite-stack";
+import * as dotenv from "dotenv";
+import * as path from "path";
 
 const app = new cdk.App();
-new NextFavoriteStack(app, 'NextFavoriteStack', {
+const env = app.node.tryGetContext("env");
+dotenv.config({ path: path.resolve(__dirname, `../.env.${env}`) });
+
+new NextFavoriteStack(app, `NextFavoriteStack-${env}`, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +20,13 @@ new NextFavoriteStack(app, 'NextFavoriteStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
+  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION },
+  userPoolName: `NextFavoriteUserPool-${env}`,
+  fromEmail: process.env.AWS_COGNITO_FROM_EMAIL!,
+  sesVerifiredDomain: process.env.AWS_COGNITO_SES_VERIFIED_DOMAIN!,
+  domainName: process.env.AWS_COGNITO_DOMAIN_NAME!,
+  callbackUrl: process.env.AWS_CALLBACK_URL!,
+  logoutUrl: process.env.AWS_LOGOUT_URL!,
+  certificateArn: process.env.AWS_CERTIFICATE_ARN!,
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/next-favorite-stack.ts
+++ lib/next-favorite-stack.ts
@@ -1,16 +1,119 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+
+// import * as cognito from "aws-cdk-lib/aws-cognito";
+
+interface NextFavoriteProps extends cdk.StackProps {
+  userPoolName: string;
+  fromEmail: string;
+  sesVerifiredDomain: string;
+  domainName: string;
+  callbackUrl: string;
+  logoutUrl: string;
+  certificateArn: string;
+}
 
 export class NextFavoriteStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: NextFavoriteProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function resource
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+            exports.handler = async function(event) {
+              return {
+                statusCode: 200,
+                body: JSON.stringify('Hello ${props.userPoolName}'),
+              };
+            };
+          `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NextFavoriteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the Lambda function URL resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    });
+
+    // Create a Cognito User Pool
+    const userPool = new cognito.UserPool(this, props.userPoolName, {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      email: cognito.UserPoolEmail.withSES({
+        fromEmail: props.fromEmail,
+        fromName: "Next Favorite",
+        sesVerifiedDomain: props.sesVerifiredDomain,
+        sesRegion: "ap-southeast-1",
+      }),
+      userVerification: {
+        emailSubject: "Your Verification Code",
+        emailBody: "Please use the following code to verify your email: {####}",
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for development,
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+      },
+    });
+
+    // Add an App Client
+    const appClient = userPool.addClient("NextFavorite", {
+      userPoolClientName: "Next Favorite",
+      authFlows: {
+        userSrp: true,
+      },
+      generateSecret: false,
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true, // Enables the Authorization Code Grant flow
+        },
+        scopes: [
+          cognito.OAuthScope.OPENID,
+          cognito.OAuthScope.EMAIL,
+          cognito.OAuthScope.PROFILE,
+        ],
+        callbackUrls: [props.callbackUrl],
+        logoutUrls: [props.logoutUrl],
+      },
+    });
+
+    // Reference an existing ACM certificate by its ARN
+    const certificateArn = props.certificateArn;
+
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      "Certificate",
+      certificateArn
+    );
+
+    // Optionally, add a domain for the hosted UI
+    const domain = userPool.addDomain("NextFavoriteDomain", {
+      customDomain: {
+        domainName: props.domainName,
+        certificate,
+      },
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, "UserPoolId", { value: userPool.userPoolId });
+    new cdk.CfnOutput(this, "AppClientId", {
+      value: appClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, "CognitoDomain", { value: domain.domainName });
   }
 }
